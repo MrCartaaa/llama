7fec4466a9c5cb5ca77d96b32a72d24beb2d6a3e
Carter Steele <carter@maetech.ca>
2026-06-05 21:10:54 -0400

    teaking

--- start-llama.sh
+++ start-llama.sh
@@ -99,7 +99,7 @@ taskset -c "$CPU_AFFINITY" "$SRV" \
     --model /home/john/.cache/llama.cpp/unsloth_Qwen3-Coder-Next-GGUF_Qwen3-Coder-Next-UD-Q4_K_S.gguf \
     --chat-template-file /home/john/llama/llama-templates/qwen3_unsloth_chat_template.jinja \
     --tensor-split $TENSOR_SPLIT \
-    --split-mode tensor \
+    --split-mode layer \
     --n-gpu-layers -1 \
     --n-cpu-moe 8 \
     --ctx-size 131072 \
@@ -111,8 +111,8 @@ taskset -c "$CPU_AFFINITY" "$SRV" \
     --flash-attn on \
     --threads $THR \
     --threads-batch $THR \
-    --batch-size 4096 \
-    --ubatch-size 2048 \
+    --batch-size 131072 \
+    --ubatch-size 65536 \
     --temp $TEMP \
     --top-p 0.95 \
     --top-k 40 \
